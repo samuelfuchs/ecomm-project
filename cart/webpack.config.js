1cbd09fd6cc91399636fce0f27c215875fc4b5b0
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-12 08:29:33 -0300

    Async Script Loading

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
             filename: 'remoteEntry.js',
             exposes: {
                 './CartShow': './src/index'
-            }
+            },
+            shared: ['faker']
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
--- products/webpack.config.js
+++ products/webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
             filename: 'remoteEntry.js',
             exposes: {
                 './ProductsIndex': './src/index',
-            }
+            },
+            shared: ['faker'],
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
